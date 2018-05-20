8f6872cb08dcf5c2cdbdd5747de8b6f3c3d967b8
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2018-05-20 17:37:59 +0430

    Remove ProductGroup since its not being used in current version

--- lib/models/product.rb
+++ lib/models/product.rb
@@ -1,12 +1,11 @@
 module Models
   class Product
-    attr_reader :product_code, :name, :price, :product_group_code
+    attr_reader :product_code, :name, :price
 
-    def initialize(product_code, name, price, product_group_code = nil)
+    def initialize(product_code, name, price)
       @product_code = product_code
       @name = name
       @price = price
-      @product_group_code = product_group_code
     end
   end
 end
